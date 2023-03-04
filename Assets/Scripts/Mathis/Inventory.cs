fb0e51956515cb8dc676c0bf947223b39c337230
Leclerc Mathis <mathis.leclerc@etudiant.univ-rennes1.fr>
2023-03-04 21:59:00 +0100

    Adding objects + inventory

---
+++ Assets/Scripts/Mathis/Inventory.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public bool Eau = false;
+    public bool Fleur = false;
+    public bool Regle = false;
+    public bool Colle = false;
+    public bool Affiche = false;
+}
