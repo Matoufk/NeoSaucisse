d30b440f8f2ecab195cd434ef4458065e34d9854
Leclerc Mathis <mathis.leclerc@etudiant.univ-rennes1.fr>
2023-03-04 11:17:27 +0100

    Adding off set to camera rotation

--- Assets/Scripts/Mathis/CameraFollow.cs
+++ Assets/Scripts/Mathis/CameraFollow.cs
@@ -8,8 +8,14 @@ public class CameraFollow : MonoBehaviour
 
     private Vector3 velocity;
 
+    public float xOffSet;
+    private void Start()
+    {
+        transform.rotation = Quaternion.Euler(xOffSet, 0, 0);
+    }
     void FixedUpdate()
     {
+        //transform.rotation = Quaternion.Euler(xOffSet, 0, 0);
         transform.position = Vector3.SmoothDamp(transform.position, player.transform.position + posOffset, ref velocity, timeOffset);
     }
 }
